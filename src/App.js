bdf567bfd89022e905548a44df3f2aaeb1b5b98b
Sabri Alabouch <sabri544@msn.com>
2022-11-25 19:21:33 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import HeaderInfos from "./components/HeaderInfos";
 
 const App = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
+
   return (
     <div className="app-container">
       <header>
--- src/components/HeaderInfos.js
+++ src/components/HeaderInfos.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 const HeaderInfos = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
   return (
     <div className="header-container">
-      <h1>header</h1>
+      <ul className="title">
+        <li>
+          <h1>
+            <img src="./assets/logo.png" alt="" /> Watch Tower
+          </h1>
+        </li>
+      </ul>
     </div>
   );
 };
