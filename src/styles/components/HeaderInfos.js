7ce676b8e99079ba6e7464ccbc147f7b54c21eb2
Sabri Alabouch <sabri544@msn.com>
2022-11-25 17:53:09 +0100

    header

---
+++ src/styles/components/HeaderInfos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderInfos = () => {
+  return (
+    <div className="header-container">
+      <h1>header</h1>
+    </div>
+  );
+};
+
+export default HeaderInfos;
