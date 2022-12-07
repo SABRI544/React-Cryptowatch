167ae292541a8fc287843f581d35e8f1c9072c96
Sabri Alabouch <sabri544@msn.com>
2022-12-07 11:02:33 +0100

    lignes du tableau

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -2,8 +2,30 @@ import React from "react";
 
 const TableLine = ({ coin, index }) => {
   return (
-    <div>
-      <h1>{coin.symbol}</h1>
+    <div className="table-line">
+      <div className="infos-container">
+        <span>*</span>
+        <p>{index + 1}</p>
+        <div className="img">
+          <img src={coin.image} heoght="20 " alt="logo" />
+        </div>
+        <div className="infos">
+          <div className="chart-img">
+            <img src="./assets/chart-icon.svg" alt="chart-icon" />
+          </div>
+          <h4>{coin.name}</h4>
+          <span>- {coin.symbol.toUpperCase()}</span>
+          <a
+            target="_blank"
+            href={
+              "https://www.coingecko.com/fr/pi%C3%A8ces/" +
+              coin.name.toLowerCase().replace(" ", "-").replace(" ", "-")
+            }
+          >
+            <img src="./assets/info-icon.svg" alt="info-icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
