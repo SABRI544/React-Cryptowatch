fb03198880b723c3ceecde05ce1b1a20a29bffbd
Sabri Alabouch <sabri544@msn.com>
2022-12-07 16:27:33 +0100

    affichage prix

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const TableLine = ({ coin, index }) => {
+  const priceFormater = (num) => {
+    if (Math.round(num).toString().length < 4) {
+      return new Intl.NumberFormat("us-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 7,
+      }).format(num);
+    } else {
+      return num;
+    }
+  };
+
   return (
     <div className="table-line">
       <div className="infos-container">
@@ -19,13 +30,18 @@ const TableLine = ({ coin, index }) => {
             target="_blank"
             href={
               "https://www.coingecko.com/fr/pi%C3%A8ces/" +
-              coin.name.toLowerCase().replace(" ", "-").replace(" ", "-")
+              coin.name
+                .toLowerCase()
+                .replace(" ", "-")
+                .replace(" ", "-")
+                .replace(" ", "-")
             }
           >
             <img src="./assets/info-icon.svg" alt="info-icon" />
           </a>
         </div>
       </div>
+      <p>{priceFormater(coin.current_price).toLocaleString()} $</p>
     </div>
   );
 };
