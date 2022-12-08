9c5b483a7eeb81829061ad5ab32dd0dcd7ccbd89
Sabri Alabouch <sabri544@msn.com>
2022-12-08 23:33:40 +0100

    staricon

---
+++ src/components/StarIcon.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const StarIcon = ({ coinId }) => {
+  const [like, setLike] = useState(false);
+
+  useEffect(() => {
+    if (window.localStorage.coinList) {
+      let favList = window.localStorage.coinList.split(",");
+      if (favList.includes(coinId)) {
+        setLike(true);
+      }
+    }
+  });
+  const idChecker = (id) => {
+    let favList = null;
+    if (window.localStorage.coinList) {
+      favList = window.localStorage.coinList.split(",");
+    }
+    if (favList) {
+      if (favList.includes(id)) {
+      }
+    } else {
+      window.localStorage.coinList = coinId;
+      setLike(true);
+    }
+  };
+
+  return (
+    <img
+      onClick={() => idChecker(coinId)}
+      src={like ? "./assets/star-full.svg" : "./assets/star-empty.svg"}
+      alt="icon-star"
+    />
+  );
+};
+
+export default StarIcon;
